b22689fb4cd94d7082533b1c3721d473e7a05eb7
Narotam-Mishra <narotam21jan@gmail.com>
2023-12-19 19:28:36 +0530

    react-dashboard_1.2

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
+import Cards from './components/Cards';
+
+import Navbar from './components/Navbar';
+import Sidebar from './components/Sidebar';
 
 function App() {
   return (
     <div className="App">
-      React Dashboard
+      <Sidebar />
+      <Navbar />
+      <Cards />
     </div>
   );
 }
